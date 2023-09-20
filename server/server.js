845abf6ec6e8b7f3856ce1d526088d6de430be74
MatheusGomesslv <101715170+matheusgomesslv222@users.noreply.github.com>
2023-09-20 10:06:42 -0300

    update

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use(express.json())
+
+app.get('/', (req, res) =>{
+    res.json({success: true});
+})
+
+app.listen(3000, ()=>{
+    console.log('Servidor Online');
+})
